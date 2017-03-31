a92486af984973a1146f665cd23ee4e3696fdcb6
kenny Huang <KennyHuang@Gangdis-MBP.home>
2017-03-31 01:40:02 -0400

    Iprogramming startup

---
+++ src/main/java/com/iprogramming/controller/iprogrammingController.java
@@ -0,0 +1,29 @@
+package com.iprogramming.controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.google.appengine.api.datastore.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+@Controller
+public class iprogrammingController {
+
+    Entity workingList;
+
+	@RequestMapping("/")
+	public String home() {
+
+		return "HomePage";
+	}
+
+
+}
---
+++ src/main/webapp/WEB-INF/pages/HomePage.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: KennyHuang
+  Date: 3/31/17
+  Time: 1:01 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Iprogramming</title>
+</head>
+<body>
+Welcome, dark blue. let's start coding.
+</body>
+</html>
